29e4bb5c99576f75650968b854cf925c2c893476
Eunju Chong <eun01300714@gmail.com>
2018-02-01 16:23:19 +0900

    #5 방 생성 채팅 구현

--- index.js
+++ index.js
@@ -2,28 +2,33 @@ var http = require('http');
 var fs = require('fs');
 var socketio = require('socket.io');
 
-var server = http.createServer(function (request, response){
+var server = http.createServer();
+
+var io = socketio.listen(server);
+
+server.listen(3000, function(){
+  console.log('Server running at http://127.0.0.1:3000');
+});
+
+server.on('request', function (request, response){
   fs.readFile('HTMLPage.html', function (error, data){
     response.writeHead(200, { 'Content-Type': 'text/html' });
     response.end(data);
   });
-}).listen(3000, function(){
-  console.log('Server running at http://127.0.0.1:3000');
 });
-var id = 0;
-var io = socketio.listen(server);
-io.sockets.on('connection', function (socket){
-  //id를 설정합니다.
-  id = socket.id;
 
-  // rint 이벤트
-  socket.on('rint', function (data){
-    //클라이언트가 전송한 데이터를 출력합니다.
-    console.log('Client Send Data: ', data);
-    //클라이언트에 smart
-    //socket.emit('smart', data); //private
-    //io.sockets.emit('smart', data);  //public
-    //socket.broadcast.emit('smart', data); //broadcast
-    io.sockets.to(id).emit('smart', data);
+io.sockets.on('connection', function (socket){
+  var roomName = null;
+
+  socket.on('join', function(data) {
+    roomName = data;
+    socket.join(data);
+
+      console.log('방 접속!:' + roomName);
+  });
+
+  socket.on('message', function(data){
+    io.sockets.in(roomName).emit('message', data);
+    console.log('메시지 도착!:' + data + '/' + roomName);
   });
 });
