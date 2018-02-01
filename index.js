8776db93a80b77f1bcd060d25bf19a227d13fde4
Eunju Chong <eun01300714@gmail.com>
2018-02-01 17:27:43 +0900

    #6 웹채팅 구현하기

--- index.js
+++ index.js
@@ -2,33 +2,18 @@ var http = require('http');
 var fs = require('fs');
 var socketio = require('socket.io');
 
-var server = http.createServer();
-
-var io = socketio.listen(server);
-
-server.listen(3000, function(){
-  console.log('Server running at http://127.0.0.1:3000');
-});
-
-server.on('request', function (request, response){
+var server = http.createServer( function (request, response){
   fs.readFile('HTMLPage.html', function (error, data){
     response.writeHead(200, { 'Content-Type': 'text/html' });
     response.end(data);
   });
+}).listen(3000, function(){
+  console.log('Server running at http://127.0.0.1:3000');
 });
+var io = socketio.listen(server);
 
 io.sockets.on('connection', function (socket){
-  var roomName = null;
-
-  socket.on('join', function(data) {
-    roomName = data;
-    socket.join(data);
-
-      console.log('방 접속!:' + roomName);
-  });
-
   socket.on('message', function(data){
-    io.sockets.in(roomName).emit('message', data);
-    console.log('메시지 도착!:' + data + '/' + roomName);
+    io.sockets.emit('message', data);
   });
 });
